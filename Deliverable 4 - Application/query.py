913e626b111c8567704c1495bc88f620b3c9e807
kool814 <karthik.s814@gmail.com>
2019-04-19 19:48:05 -0400

    Adding minStockByState

--- Deliverable 4 - Application/query.py
+++ Deliverable 4 - Application/query.py
@@ -5,6 +5,16 @@ connection_string = "host='localhost' dbname='stocks' user='stocks' password='st
 conn = psycopg2.connect(connection_string)
 cursor = conn.cursor()
 
-cursor.execute("SELECT * FROM prices")
-records = cursor.fetchone()
-print records
+def minStockByState(state, minAmount, date):
+    cursor.execute("SELECT DISTINCT prices.symbol, prices.date_, prices.close, securities.Address, securities.Security FROM securities, prices WHERE securities.Ticker = prices.symbol;")
+    records = cursor.fetchall()
+    stocks=[]
+
+    for r in records:
+        if state.lower() in r[3].lower() and str(r[1]) == date and str(r[2]) >= minAmount:
+            stocks.append( (r[0], r[4], str(r[2])) )
+    print stocks
+
+
+if __name__ == '__main__':
+    minStockByState("CA", "20", '2016-01-06')
