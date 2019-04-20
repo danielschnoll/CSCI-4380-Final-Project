5b837cd0dca722f959fb9ef26729e5e97200a7fd
kool814 <karthik.s814@gmail.com>
2019-04-19 20:24:05 -0400

    Adding highest stock price by date

--- Deliverable 4 - Application/query.py
+++ Deliverable 4 - Application/query.py
@@ -3,9 +3,9 @@ import unittest
 
 connection_string = "host='localhost' dbname='stocks' user='stocks' password='stocks'"
 conn = psycopg2.connect(connection_string)
-cursor = conn.cursor()
 
 def minStockByState(state, minAmount, date):
+    cursor = conn.cursor()
     cursor.execute("SELECT DISTINCT prices.symbol, prices.date_, prices.close, securities.Address, securities.Security FROM securities, prices WHERE securities.Ticker = prices.symbol;")
     records = cursor.fetchall()
     stocks=[]
@@ -13,7 +13,21 @@ def minStockByState(state, minAmount, date):
     for r in records:
         if state.lower() in r[3].lower() and str(r[1]) == date and str(r[2]) >= minAmount:
             stocks.append( (r[0], r[4], str(r[2])) )
-    print stocks
+    return stocks
+
+def highestStockPriceByDate(date):
+    cursor = conn.cursor()
+    sql = """
+        SELECT DISTINCT securities.Ticker, securities.Security, prices.high
+        FROM securities, prices 
+        WHERE prices.high = 
+            (select max(high) from prices 
+            WHERE prices.date_ = '{}')
+        AND securities.Ticker = prices.symbol;
+    """.format(date)
+    cursor.execute(sql)
+    records = cursor.fetchall()
+    return records
 
 def findMaxMinForIndustry(industry):
 	cursor = conn.cursor()
@@ -28,4 +42,5 @@ def findMaxMinForIndustry(industry):
 
 
 if __name__ == '__main__':
-    minStockByState("CA", "20", '2016-01-06')
+    # minStockByState("CA", "20", '2016-01-06')
+    print(highestStockPriceByDate('2016-01-07'))
